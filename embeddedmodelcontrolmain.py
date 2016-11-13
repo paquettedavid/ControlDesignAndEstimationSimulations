382229169a5851b90c17d9ce592dcfc5d910cff2
David Paquette <david.paquette@uconn.edu>
2016-11-13 17:23:14 -0500

    started working on some embedded model control design simulations
    currently have a size mismatch issue during multiplication in the state
    transition process

--- embeddedmodelcontrolmain.py
+++ embeddedmodelcontrolmain.py
@@ -13,37 +13,43 @@ def main():
     A = np.matrix('-1') # dynamics at operating point
     B = np.matrix('1')  # input gain matrix
     C = np.matrix('1')  # measurement gain matrix
-    # continuous LQR parameters
-    Q = np.matrix('1') # state penalty
-    R = np.matrix('1') # control penalty
 
-    # discrete dynamics (computed using continuous system)
-    Ad = np.exp(A*Ts)
-    Bd = (Ad-np.eye(np.shape(A)[0]))*B*np.linalg.inv(A)
-    Cd = C
+    # embed control model for ramp tracking
+    Ae = np.matrix([[0, 1, 0],[0, 0, C],[0, 0, A]])
+    Be = np.matrix([[0],[0],[B]])
+    Ce = np.matrix(np.shape(Ae)[0])
+    # LQR "gains"
+    Qe = np.matrix([[1, 0, 0],[0, 1, 0],[0, 0, 0]]) # state penalty
+    Re = np.matrix('1') # control penalty
+
+    # discrete dynamics (computed using embedded continuous system)
+    Ad = sp.linalg.expm(Ae*Ts)
+    if np.linalg.det(Ae) != 0: # if Ae is not singular
+        Bd = (Ad - np.eye(np.shape(Ae)[0])) * Be * np.linalg.inv(Ae)
+    else: # if singular, use pseudo inverse
+        Bd = np.linalg.pinv(Ae)*(Ad-np.eye(np.shape(Ae)[0]))*Be
+    Cd = Ce
     # discrete LQR parameters
-    Qd = Q*Ts
-    Rd = R/Ts
+    Qd = Qe*Ts
+    Rd = Re/Ts
 
     # solve for optimal LQR gain
     P = sp.linalg.solve_discrete_are(Ad,Bd,Qd,Rd) # solve Riccati equation
-    Klqr = np.linalg.inv(R)*np.transpose(B)*P # compute gain
-
+    Klqr = np.linalg.inv(np.transpose(Bd)*P*Bd+Rd)*(np.transpose(Bd)*P*Ad)
     # specify state, input and output signals
-    x = np.matrix('1') # state, x(0)=1
-    R = np.matrix([[0]])  # input, regulate to 0 for all time
+    x = np.matrix([[0],[0],[0]]) # state, x(0)=1
+    R = np.matrix([[0],[0],[1]])  # input, regulate to 0 for all time
     u_sig = np.zeros((n,1))
     y_sig = np.zeros((n, 1))
 
     for k in range(0,n):
         #  optimal regulator using LQR state feedback
-        U = (R-x)*Klqr # state feedback
-        x = Ad*x + Bd*U # compute next state
-        y = Cd*x # compute output
+        U = np.multiply((R-x),Klqr) # state feedback
+        x = np.multiply(Ad,x) + np.multiply(Bd,U) # compute next state
+        y = np.multiply(Cd,x) # compute output
 
-        u_sig[k] = U.T
-        y_sig[k] = y.T # "sample"/measure output from system
-
+        u_sig[k] = U[2,2].T
+        y_sig[k] = y[2,2].T # "sample"/measure output from system
 
     plt.plot(t, y_sig)  # plot output
     plt.plot(t, u_sig)  # plot controller effort
