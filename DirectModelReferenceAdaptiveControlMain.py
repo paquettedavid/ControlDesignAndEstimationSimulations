b1148ec918058d1f11aff2056657c18effa8467c
David Paquette <dpaquette01@gmail.com>
2017-07-19 19:38:59 -0400

    model reference adaptive control example
    of a first order system (without state feedback)

---
+++ DirectModelReferenceAdaptiveControlMain.py
@@ -0,0 +1,78 @@
+import numpy as np
+import scipy as sp
+from scipy import linalg
+import matplotlib.pyplot as plt
+
+def main():
+    tf = 10 # final time
+    Ts = 0.01 # sample rate
+    t = np.arange(0,tf,Ts) # t0 to tf in increments of Ts
+    n = np.size(t)
+
+    # continuous plant with uncertain dynamics
+    A = np.matrix('-1.5') # dynamics at operating point
+    B = np.matrix('1.2')  # input gain matrix
+    C = np.matrix('1')  # measurement gain matrix
+    x = np.matrix('0')  # state, x(0)=1
+
+    # continuous reference model
+    A_ref = np.matrix('-1')
+    B_ref = -1*A_ref # for unity DC gain across controller
+    x_ref = np.matrix('0')
+    r = np.matrix('2') # 'setpoint'
+
+    # continuous model reference adaptive controller
+    # u = k_x_hat*x + k_r_hat*r
+    # adaptive parameters
+    k_x_hat = np.matrix('0')
+    k_r_hat = np.matrix('0')
+    gamma_x = np.matrix('15')
+    gamma_r = np.matrix('15')
+
+    # discrete plant dynamics (computed using continuous system)
+    Ad = np.exp(A*Ts)
+    Bd = (Ad-np.eye(np.shape(A)[0]))*B*np.linalg.inv(A)
+    Cd = C
+
+    # compute discrete reference model
+    A_ref_d = np.exp(A_ref * Ts)
+    B_ref_d = (A_ref_d - np.eye(np.shape(A_ref)[0])) * B_ref * np.linalg.inv(A_ref)
+
+    # data collection I/O signals
+    u_sig = np.zeros((n,1))
+    y_sig = np.zeros((n, 1))
+    k_x_sig = np.zeros((n, 1))
+    k_r_sig = np.zeros((n, 1))
+
+    for k in range(0,n):
+
+        #  compute controller output using estimated gains (input to our system)
+        U = k_x_hat*x + k_r_hat*r # feedback and feedforward controller
+
+        # simulate uncertain(to the controller) plant
+        x = Ad*x + Bd*U # compute next state
+        y = Cd*x # compute output
+
+        # update reference model
+        x_ref = A_ref_d*x_ref + B_ref_d*r
+
+        # estimate gains using the following adaptive laws
+        k_x_hat = np.matrix(-1)*gamma_x*x*(x - x_ref)*np.sign(B)
+        k_r_hat = np.matrix(-1)*gamma_r*r*(x - x_ref)*np.sign(B)
+
+        # "sample"/measure output from system for plotting
+        k_x_sig[k] = k_x_hat.T
+        k_r_sig[k] = k_r_hat.T
+        u_sig[k] = x_ref.T
+        y_sig[k] = y.T
+
+
+    plt.plot(t, y_sig, '-b', label= 'plant output')
+    plt.plot(t, u_sig, '-r', label= 'desired track')
+    plt.plot(t, k_x_sig, '-g', label='estimated k_x')
+    plt.plot(t, k_r_sig, '-y', label='estimated k_r')
+    plt.legend(loc='lower right')
+    plt.show()
+
+if __name__ == "__main__":
+    main()
